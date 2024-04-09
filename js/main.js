de5925916c4c09b34e17a4c9f414c68d978a1158
Ludmila Zabrovskaya <forex79637785441@yandex.ru>
2024-04-09 20:47:10 +0300

    Изменила код функции getRandomInteger (более простой вариант без проверки min и max)

--- js/main.js
+++ js/main.js
@@ -29,10 +29,7 @@ const names = [
 ];
 
 const getRandomInteger = (min, max) => {
-  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
+  Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 const createRandomIdFromRangeGenerator = (min, max) => {
