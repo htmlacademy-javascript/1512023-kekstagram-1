b55c32fdc98fab52b72bde7244607574e0b492d9
Ludmila Zabrovskaya <forex79637785441@yandex.ru>
2024-05-01 23:42:20 +0300

    исправлены ошибки в модуле 7

--- js/draw-miniatures.js
+++ js/draw-miniatures.js
@@ -1,21 +1,23 @@
-import {photos} from './create-photos.js';
+//import {photos} from './create-photos.js';
 const thumbnailsContainer = document.querySelector('.pictures');
 const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const thumbnailsList = photos();
-const thumbnailsFragment = document.createDocumentFragment();
-
-thumbnailsList.forEach(({url, description, likes, comments}) => {
+const createThumbnail = ({url, description, likes, comments}) =>{
   const thumbnailElement = thumbnailTemplate.cloneNode(true);
   thumbnailElement.querySelector('.picture__img').src = url;
   thumbnailElement.querySelector('.picture__img').alt = description;
   thumbnailElement.querySelector('.picture__likes').textContent = likes;
-  thumbnailElement.querySelector('.picture__comments').textContent = comments;
+  thumbnailElement.querySelector('.picture__comments').textContent = comments.length;
+  return thumbnailElement;
+};
 
-  thumbnailsFragment.appendChild(thumbnailElement);
+const createThumbnailList = (photos) => {
+  const thumbnailsFragment = document.createDocumentFragment();
+  photos.forEach((photo) => {
+    const thumbnail = createThumbnail(photo);
+    thumbnailsFragment.append(thumbnail);
+  });
+  thumbnailsContainer.append(thumbnailsFragment);
+};
 
-});
-
-thumbnailsContainer.appendChild(thumbnailsFragment);
-
-
+export {createThumbnailList};
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import {photos} from './create-photos.js';
 //eslint-disable-next-line no-console
 console.log(photos);
-
+import {createThumbnailList} from './draw-miniatures.js';
+createThumbnailList(photos);
